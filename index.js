8af37347ebfb020f19c8e13da7f8e9276d4e0991
Shovgur <s.showgur@yandex.by>
2022-08-08 12:01:48 +0300

    added todo

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const addMessage = document.querySelector(".message");
 const addButton = document.querySelector(".add");
 const todo = document.querySelector(".todo");
+const delBtn = document.querySelector(".del");
 
 let todoList = [];
 
@@ -27,7 +28,8 @@ function displayMessages() {
     displayMessages += `
     <li>
     <input type= 'checkbox' id= 'item_${i}'${item.checked ? "checked" : ""}>
-    <label for= 'item_${i}'>${item.todo}</label>
+    <label for= 'item_${i}'>${item.todo} </label>
+    <button class="del">X</button>
     </li>
     `;
 
@@ -40,11 +42,18 @@ todo.addEventListener("change", function (event) {
     "[for=" + event.target.getAttribute("id") + "]"
   ).innerHTML;
 
-todoList.forEach(function(item){
-    if(item.todo === valueLabel){
-        item.checked = !item.checked 
-        localStorage.setItem('todo', JSON.stringify(todoList))
+  todoList.forEach(function (item) {
+    if (item.todo === valueLabel) {
+      item.checked = !item.checked;
+      localStorage.setItem("todo", JSON.stringify(todoList));
     }
-})
+  });
+});
 
+delBtn.addEventListener("click", function (event) {
+  todoList.forEach(function (index) {
+    if(event.target){
+      todoList.splice(i, 1);
+    }
+  });
 });
